41818fc530b8ae535712c5905c0a13f435d4df9b
SYED MUSA RAZA <79834398+syedmusa-eng@users.noreply.github.com>
2021-10-05 18:44:28 +0200

    Making student data type.
    Using classes making a new data type with a bunch of useful info about that data type.

---
+++ Classes_and_objects.py
@@ -0,0 +1,9 @@
+from Student import Student
+
+#student1 is an actual student of the class Student, its an intance of the class.
+student1 = Student("Jim", "Business", 3.1, False)
+student2 = Student("pam", "Art", 2.5, True)
+
+print(student1.name)
+print(student2.name)
+
